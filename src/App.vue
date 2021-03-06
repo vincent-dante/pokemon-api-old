02ca548b1640bb7da39b64b99f29a52b43231a8b
Vincent <vincent.dante@gmail.com>
2021-03-06 19:42:29 +0800

    design homepage

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
-<template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+<template>  
+  <div id="app-container">
+    <router-view/>
   </div>
-  <router-view/>
+
+  <div class="background-container">
+    <img src="./assets/background.gif" alt="" srcset="">  
+  </div>  
 </template>
 
 <style lang="scss">
+html, body {
+  padding: 0;
+  margin: 0;
+}
+
+
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -15,16 +24,44 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
+.app-container {
+  position: relative;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+.background-container {
+  position: relative;
+  z-index: -1;
+  overflow: hidden;
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  img {
+    position: fixed;
+    top: 0;
+    left: -120px;
+    right: 0;
+    bottom: 0;
+    height: 100%;
+    width: 1100px;
+  }  
+}
+
+@media only screen and (min-width: 768px) {
+  .background-container {
+
+    img {
+      left: 0;
+      width: 100%;
+    }  
   }
+}
+
+@media only screen and (min-width: 992px) {
+  .background-container {
+
+    img {
+      height: 100%;
+      left: 0;
+      width: 100%;
+    }  
+  } 
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,77 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="logo-container">
+   <img src="../assets/pokemon-logo.png" alt="" srcset="" class="logo">
   </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <input type="text" class="form-control search-input" placeholder="Search Pokemon Here!">        
+        <input type="button" value="Search" class="search-btn">
+      </div>
+    </div>
+
+  </div>
+  
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
+
+<style lang="scss">
+
+
+@keyframes bounce {
+  0%, 100% {
+    bottom: 0;
+  }
+  50% {
+    bottom: 10px;
+  }
+}
+
+.logo-container {
+  position: relative;
+  bottom: 0;
+  animation: bounce 1.2s linear infinite;  /* animation set */
+}
+
+.logo {
+  width: 250px;
+}
+
+.search-input, 
+.search-btn 
+{
+  display: inline-block;
+}
+
+.search-input {
+  width: 70%;
+  margin: 0 auto;
+  font-family: 'DotGothic16', sans-serif;
+}
+
+.search-btn {
+  width: 20%;
+  margin-left: 10px;
+  font-family: 'DotGothic16', sans-serif;
+}
+
+@media only screen and (min-width: 768px) {
+  .search-input {
+    width: 30%;
+  }
+
+  .search-btn {
+    width: 10%;
+    margin-left: 20px;
+    font-family: 'DotGothic16', sans-serif;
+  }
+}
+
+</style>
